029f58b2dc6c1672daad918f8a9c4908671bb4d5
Jacob Nadal <jnadal14@student.ubc.ca>
2025-03-09 14:36:27 -0700

    web-app skeleton created

---
+++ web_app/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Movie Review Corpus</h1>
+      <p>Search and explore annotated movie reviews.</p>
+    </div>
+  );
+}
+
+export default App;
